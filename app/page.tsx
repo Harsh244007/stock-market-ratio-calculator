67bdae73886331eb177f15e2e9a4614fe588425b
Harsh Patel <Patelharsh241999@gmail.com>
2023-11-21 01:35:42 +0530

    * Initial commit.

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,82 @@
-import Image from 'next/image'
+"use client";
+import { ChangeEvent, useState } from "react";
+const RatioCalculator = () => {
+  const [value, setValue] = useState("1000");
+  const [profit, setProfit] = useState("2");
+  const [loss, setLoss] = useState("1");
+  const [ratioResult, setRatioResult] = useState<string | null>(null);
 
-export default function Home() {
+  const handleCalculate = () => {
+    const finalProfit = Number(profit);
+    const finalTotal = Number(profit) + Number(loss);
+    const finalProfitPer = ((finalProfit * 100) / finalTotal).toFixed(2);
+    const finalLoss = Number(loss);
+    const finalLossPer = ((finalLoss * 100) / finalTotal).toFixed(2);
+    const showProfit = Math.round((+finalProfitPer * +value) / 100);
+    const showLoss = Math.round((+finalLossPer * +value) / 100);
+    const firstLine = `<p>If you take 10 trades with ${value}</p>`;
+    const secondLine = `<p>and You profit  ${showProfit} with win-percentage of ${finalProfitPer} % </p>`;
+    const thirdLine = `<p>and rest ${showLoss} losses you ${finalLossPer} % </p>`;
+    const fourthLine = `<p>Then your stratgy is ${finalProfitPer} % profitable.</p>`;
+    setRatioResult(`${firstLine} ${secondLine} ${thirdLine} ${fourthLine}`);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="w-full max-w-lg mx-auto h-screen flex items-center justify-center flex-col gap-4 p-2">
+      <h2>Ratio Calculator</h2>
+      <div className="mb-4">
+        <label htmlFor="value" className="block text-sm font-medium text-white-700 mb-1">
+          Value
+        </label>
+        <input
+          type="number"
+          id="value"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          value={value}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="mb-4">
+        <label htmlFor="profit" className="block text-sm font-medium text-white-700 mb-1">
+          Profile (1-10)
+        </label>
+        <input
+          type="number"
+          id="profit"
+          min="1"
+          max="10"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          value={profit}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setProfit(e.target.value)}
+        />
+        <p className="text-xs text-gray-500">Enter a value between 1 and 10 for Profit</p>
       </div>
+      <div className="mb-4">
+        <label htmlFor="loss" className="block text-sm font-medium text-white-700 mb-1">
+          Loss (1-10)
+        </label>
+        <input
+          type="number"
+          id="loss"
+          min="1"
+          max="10"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          value={loss}
+          onChange={(e) => setLoss(e.target.value)}
+        />
+        <p className="text-xs text-gray-500">Enter a value between 1 and 10 for Loss</p>
+      </div>
+      <div className="mb-4">
+        <button
+          onClick={handleCalculate}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Calculate Ratio
+        </button>
+      </div>
+      {ratioResult && <div className="text-sm text-white-700" dangerouslySetInnerHTML={{ __html: ratioResult }} />}
     </main>
-  )
-}
+  );
+};
+
+export default RatioCalculator;
